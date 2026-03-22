dac9bb77e5f867df82648bbfe709a8c41dda6c4d
nyyakko <nyakonyns@gmail.com>
2026-03-22 14:23:47 -0300

    ignore node_modules on build.sh

--- _meta/scripts/build.sh
+++ _meta/scripts/build.sh
@@ -4,11 +4,10 @@ echo "Gerando PDF"
 
 if docker \
     run --rm -v ./:/app -v ./build:/app/build \
-        pandoc $(find . -path ./_meta -prune , -name "*.md" | sort) ./_meta/_referencias.md \
+        pandoc $(find . -path ./node_modules -prune , -path ./_meta -prune , -name "*.md" | sort) ./_meta/_referencias.md \
         --bibliography ./src/bibliografia.bib \
         -o build/output.pdf; then
     echo "O PDF foi gerado com sucesso"
 else
     echo "Falhas ao gerar o PDF"
 fi
-
